13317556a472810287120ba749cb6d1dc44db304
LourencoLegend <lourencolegend@yahoo.com>
2022-05-24 08:08:36 -0600

    fixed htmand css missing parts

---
+++ Assignments/chamber-of-commerce/js/scripts.js
@@ -0,0 +1,45 @@
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const todaysdate = new Date();
+const dayName = daynames[todaysdate.getDay()];
+const monthName = months[todaysdate.getMonth()];
+const year = todaysdate.getFullYear();
+const currentdate = `${dayName},  ${todaysdate.getDate()} ${monthName} ${year}`;
+document.getElementById('year').textContent = year;
+document.getElementById('currentdate').textContent = currentdate;
+
+//last modified date and time
+let d = new Date(document.lastModified)
+let date = `${d.getMonth()+1}/${d.getDate()}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+document.getElementById("lastmod").textContent = date
+
+// hamburger
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+
+}
+
+const x = document.getElementById('hamburgerBtn')
+x.onclick = toggleMenu;
