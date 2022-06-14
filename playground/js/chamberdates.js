ac759caac55d84bf07793b20aaea72eeea20d458
LourencoLegend <lourencolegend@yahoo.com>
2022-06-14 08:42:05 -0600

    Files and folder removed and updated

--- playground/js/chamberdates.js
+++
@@ -1,9 +0,0 @@
-const now = new Date();
-const today = document.querySelector("#today");
-const copyrightyear = document.querySelector("#copyrightyear");
-const lastupdate = document.querySelector("#lastupdate");
-
-today.textContent = new Intl.DateTimeFormat("en-UK", { dateStyle: "full",}).format(now);
-
-copyrightyear.textContent = now.getFullYear();
-lastupdate.textContent = document.lastModified;
