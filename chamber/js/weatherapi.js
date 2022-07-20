8217b8af1049e944e4a442a2cffdfde968919610
LourencoLegend <lourencolegend@yahoo.com>
2022-07-20 08:53:46 -0600

    update

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -4,41 +4,27 @@ const weatherIcon = document.querySelector('#weather-icon');
 const wind_speed = document.getElementById("wind");
 var wind_chill = document.getElementById("wind_chill");
 const captionDesc = document.querySelector('figcaption');
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=SaoPaulo&units=imperial&appid=4a560932da68c6d09051ac9825c6d318'
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Orem&appid={4a560932da68c6d09051ac9825c6d318}'
+
+  
+apiFetch(url);
+
+async function apiFetch(apiURL) {
+    const response = await fetch(apiURL);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+};
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `${weatherData.main.temp.toFixed(0)}`;
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
+
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
-    if (currentTemp <= 50 && wind_speed > 3) {
-      let wind_chill_math = (35.74 + (0.6215 * currentTemp))-(35.75 * Math.pow(wind_speed,0.16)) + (0.4275*currentTemp*Math.pow(wind_speed,0.16));   
-      let wind_chilltemp = Math.round((wind_chill_math + Number.EPSILON)*10)/100;
-      wind_chill.textContent = `Wind Chill: ${wind_chilltemp}`;
-    } else {
-        wind_chill.textContent = "Wind Chill: N/A";
-    }
-
-    wind_speed.textContent = `Wind Speed: ${weatherData.wind.speed} m/s`;
     weatherIcon.setAttribute('src', iconsrc);
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
-
-
-async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        //console.log(data); // this is for testing the call
-        displayResults(data);
-      } else {
-          throw Error(await response.text());
-      }
-    } catch (error) {
-        console.log(error);
-    }
-}
-  
-apiFetch();
-
